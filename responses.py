98b9dcc11b613b3e7a3373a3f611e7219fd773ec
tamashe-dev <sofyan93130@gmail.com>
2024-11-06 17:05:18 +0100

    brooooooo

--- responses.py
+++ responses.py
@@ -9,7 +9,7 @@ def get_response(user_input: str) -> str:
 
         if lowered == '':
                 return 'Leeech mabte7kiniich brooo'
-        elif 'salam' in lowered:
+        elif 'salam','bonjour','wsh','bjr','slm','hello' in lowered:
                 return 'Salaam brooooo!'
         else:
                 return choice(['Broo je n\'ai pas compris...',
